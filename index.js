7c8677a4dd233b857036dc1a8f320f0ec5b7b305
Bert Spaan <bert.spaan@gmail.com>
2017-04-21 16:33:45 -0400

    gets modules from spacetime-etl

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const path = require('path')
 const H = require('highland')
 const R = require('ramda')
 const graphlib = require('graphlib')
-
-const config = require('spacetime-config')
 const etl = require('spacetime-etl')
 
 const argv = require('minimist')(process.argv.slice(2))
@@ -35,47 +31,7 @@ if (!argv._ || argv._.length !== 1 || !modes.includes(argv._[0])) {
 
 const mode = argv._[0]
 
-const modulePrefix = config.etl.modulePrefix
-const moduleDir = config.etl.moduleDir
-
-function isDirectory (module) {
-  if (module.dir === '.') {
-    return false
-  }
-
-  var stat = fs.statSync(path.join(module.baseDir, module.dir))
-  return stat.isDirectory()
-}
-
-function getFilename (type, module) {
-  return path.join(module.baseDir, module.dir, `${module.id}.${type}`)
-}
-
-function containsFile (type, module) {
-  return fs.existsSync(getFilename(type, module))
-}
-
-function allModules (baseDir, type) {
-  if (!type) {
-    return []
-  }
-
-  return fs.readdirSync(baseDir)
-    .map((file) => ({
-      id: file.replace(modulePrefix, ''),
-      dir: file,
-      baseDir: baseDir
-    }))
-    .filter(isDirectory)
-    .filter(R.curry(containsFile)('js'))
-    .filter(R.curry(containsFile)('dataset.json'))
-    .map((module) => Object.assign(module, {
-      dataset: require(getFilename('dataset.json', module)),
-      steps: require(getFilename('js', module)).steps.map((fn) => fn.name)
-    }))
-}
-
-const modules = allModules(moduleDir, '.js')
+const modules = etl.modules()
 
 // Add all modules, and their steps
 modules.forEach((module) => {
@@ -131,10 +87,10 @@ if (mode === 'list') {
   }, null, 2))
 } else if (mode === 'run') {
   // Curried ETL function, with logging enabled
-  const curriedEtl = R.curry(etl)(R.__, R.__, true)
+  const curriedExecute = R.curry(etl.execute)(R.__, R.__, true)
 
   H(getList())
-    .map(curriedEtl)
+    .map(curriedExecute)
     .nfcall([])
     .series()
     .stopOnError((err) => {
